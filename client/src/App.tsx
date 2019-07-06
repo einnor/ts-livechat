0dc821c73a31114daa9eb556799e904a898df08c
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-07-06 13:51:02 +0300

    App class with initial state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ChatMessage, ChatState } from './types';
+import { ChatContext } from './ChatContext';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  static contextType = ChatContext;
+
+  state: ChatState = {
+    messages: [
+      {
+        message: 'Welcome! Type a message and press Send Message to continue the chat.',
+        author: 'Bot'
+      }
+    ],
+    input: '',
+  }
 }
 
 export default App;
