a687ae6039b78c3f042248f3fed1d43a32b97e9e
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-07-06 14:32:39 +0300

    Constructor to initialize the fields, initiate the socket connection and start listening

--- backend/src/ChatServer.ts
+++ backend/src/ChatServer.ts
@@ -11,4 +11,14 @@ export class ChatServer {
     private server: Server;
     private io: SocketIO.Server;
     private port: string | number;
+
+    constructor () {
+        this._app = express();
+        this.port = process.env.PORT || ChatServer.PORT;
+        this._app.use(cors());
+        this._app.options('*', cors());
+        this.server = createServer(this._app);
+        this.initSocket();
+        this.listen();
+    }
 }
